a97ab22e83662ec474c596fe059b1c003b2b72e0
anandg112 <anandg112@gmail.com>
2018-03-25 00:12:47 -0400

    updated child process file

--- filesystem/watcher-spawn.js
+++ filesystem/watcher-spawn.js
@@ -5,6 +5,13 @@ const spawn = require('child_process').spawn
 const filename = process.argv[2];
 
 if (!filename) {
-
+   throw Error('A file to watch must be specified');
 }
+
+fs.watch(filename, () => {
+   const ls = spawn('ls', ['-l', '-h', filename]);
+   ls.stdout.pipe(process.stdout);
+});
+
+console.log(`Now watching ${filename} for changes ...`);
 
